da10ff7f4ea6779378dc6b9128f5297b04566e1d
IZEGHOUINE redouane <izegh.nizar@gmail.com>
2025-01-14 23:02:24 +0100

    Revert "Modification"
    This reverts commit c4fa3d040cd6806f61a14353e734594788afc5c6.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Yes Yes");
+Console.WriteLine("Hello, c## hello!");
+Console.WriteLine("I agree with you");
 
+Console.WriteLine("Fetch");
+
+
+
+Console.WriteLine("Fetch Three");
 
