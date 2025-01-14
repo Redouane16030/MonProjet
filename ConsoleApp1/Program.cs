7fe36a4c6c1eb7a87ee82e830b1e2de2efe370f6
IZEGHOUINE redouane <izegh.nizar@gmail.com>
2025-01-14 23:02:43 +0100

    yes

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, c## hello!");
-Console.WriteLine("I agree with you");
 
-Console.WriteLine("Fetch");
-
-
-
-Console.WriteLine("Fetch Three");
 
