8e77292b4d75d40bddc9e603fccadd6ce4b97d5e
IZEGHOUINE redouane <izegh.nizar@gmail.com>
2025-01-14 22:45:25 +0100

    I add thhis fonctionality

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, c## hello!");
 Console.WriteLine("I agree with you");
+
+Console.WriteLine("Fetch");
+
+Console.WriteLine("Fetch Twoo");
+
